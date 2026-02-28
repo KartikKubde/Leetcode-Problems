7460da64d4f526a990ca6f1c904e3d54573833ce
KartikKubde <152711515+KartikKubde@users.noreply.github.com>
2026-03-01 00:22:03 +0530

    Add files via upload

---
+++ 01Valid_Parenthesis.py
@@ -0,0 +1,27 @@
+# LC 20. Valid Parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for i in range(len(s)):
+
+            if(s[i] == '(' or s[i] == '[' or s[i] == '{'):
+                stack.append(s[i])
+            
+            else:
+                if(len(stack) == 0):
+                    return False
+                ch = stack.pop()
+
+                if(
+                    (s[i] == ")" and ch == "(")
+                    or (s[i] == "]" and ch == "[")
+                    or (s[i] == "}" and ch == "{")
+                ):
+                    continue
+                
+                else:
+                    return False
+
+        return len(stack) == 0
