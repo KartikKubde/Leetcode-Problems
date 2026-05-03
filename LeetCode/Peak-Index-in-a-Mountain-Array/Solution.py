a325188cbd42178f55fa8f2098a44581f9cee5b5
KartikKubde <152711515+KartikKubde@users.noreply.github.com>
2026-05-03 22:58:52 +0530

    Add solution for Peak Index in a Mountain Array

--- LeetCode/Peak-Index-in-a-Mountain-Array/Solution.py
+++ LeetCode/Peak-Index-in-a-Mountain-Array/Solution.py
@@ -1,22 +1,16 @@
-1class Solution {
-2public:
-3    int peakIndexInMountainArray(vector<int>& arr) {
-4       int s=0;
-5       int e=arr.size() - 1;
-6
-7       int mid=s+(e-s)/2;
-8
-9       while(s<e){
-10        if (arr[mid]<arr[mid+1]){
-11            s=mid+1;
-12        }
-13        else{
-14            e=mid;
-15        }
-16        mid=s+(e-s)/2;
-17       }
-18       return s;
-19
-20
-21    }
-22};
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+    l = 1
+    r = n - 2
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m-1] < arr[m] > arr[m+1]:
+            return m
+        elif arr[m-1] > arr[m]: 
+            # arr[m-1] > arr[m] > arr[m+1] ==> search left, since we are in right region
+            r = m - 1
+        else: 
+            # arr[m-1] < arr[m] < arr[m+1] ==> search right, since we are in left region
+            l = m + 1 
+        
+    return -1
